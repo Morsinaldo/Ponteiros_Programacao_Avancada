763e5701c0e15b9eb23a868c840953171c4a89e0
Morsinaldo <morsinaldo.js@outlook.com>
2021-06-21 00:07:12 -0300

    Questao 14

---
+++ questao14/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Assinatura das funções
+int compare (const void *a, const void *b);
+
+int main(){
+
+    //&: retorna o ENDEREÇO da variável
+    //*: recupera o CONTEÚDO DO ENDEREÇO
+
+    int n;
+    float *v;
+
+    printf("Digite quantos números serão informados: ");
+    scanf("%d", &n);
+
+    // Preenchendo o array de alocação de memória dinâmica
+    v = malloc(n*sizeof(float));
+    for(int i = 0; i < n; i++){
+        scanf("%f", &v[i]);
+    }
+
+    qsort(v, n, sizeof(float), compare); // Chamando a função qsort
+
+    // Imprimindo os números
+    for(int i = 0; i < n; i++){
+        printf("%f ", v[i]);
+    }
+
+    free(v); // Liberação da memória alocada
+    return 0;
+}
+
+// Funções
+int compare (const void *a, const void *b){
+    if(*(float*)a < *(float*)b){
+    return -1;
+  }else if(*(float*)a > *(float*)b){
+    return 1;
+  }else{
+    return 0;
+  }
+}
