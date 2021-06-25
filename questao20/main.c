94644b7e2efcb0fada1f5316162dfdb82146cf41
Morsinaldo <morsinaldo.js@outlook.com>
2021-06-25 13:29:45 -0300

    Questao 20

--- questao20/main.c
+++ questao20/main.c
@@ -10,7 +10,7 @@ int main(){
 
     clock_t t1Inicial, t1Final, t2Inicial, t2Final;
 
-    long int n = 10000000;
+    long int n = 9999999999;
     int *p;
 
     t1Inicial = clock(); // Inicio da medição imediatamente antes da alocação dinâmica de memória sem o garbage collector
@@ -20,7 +20,7 @@ int main(){
         free(p);
     }
 
-    t1Final = clock() - t1Inicial; // Fim da medição imediatamente depois da alocação/liberação da memória dinâmica
+    t1Final = clock(); // Fim da medição imediatamente depois da alocação/liberação da memória dinâmica
 
     t2Inicial = clock(); // Inicio da medição imediatamente antes da alocação usando a função GC_MALLOC da biblioteca libgc-dev
 
@@ -28,10 +28,14 @@ int main(){
         p = GC_MALLOC(sizeof(int));
     }
 
-    t2Final = clock() - t2Inicial; // Fim da medição imediatamente depois da alocação usando a biblioteca libgc
+    t2Final = clock(); // Fim da medição imediatamente depois da alocação usando a biblioteca libgc
 
-    printf("Tempo de execução da alocação dinâmica de memória: %lf \n", (double)t1Final/CLOCKS_PER_SEC);
-    printf("Tempo de execução da alocação com a biblioteca LibGC: %lf \n", (double)t2Final/CLOCKS_PER_SEC);
+    double t1Execucao = (double)((t1Final - t1Inicial)/CLOCKS_PER_SEC);
+    double t2Execucao = (double)((t2Final - t2Inicial)/CLOCKS_PER_SEC);
+
+    printf("Tempo de execução da alocação dinâmica de memória: %lf \n", t1Execucao);
+    printf("Tempo de execução da alocação com a biblioteca LibGC: %lf \n", t2Execucao);
 
     return 0;
 }
+
